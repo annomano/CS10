02e2248ae2a3a8863ce9278c27a9fe2b02b98c0b
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-14 15:12:44 -0600

    This is my newer assignment

--- Credit 3/src/Assignments/Assignment1.java
+++ Credit 3/src/Assignments/Assignment1.java
@@ -1,10 +1,27 @@
 package Assignments;
+/*
 
-public class Assignment1 {
+Program: Assignment1.java          Date: 4/14/2022
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+Author: Steven On 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+public class Assignment1
+{
+	
+	public static void main(String[] args) 
+	{
+		
+	       System.out.println("From: Mr. Wilson");            
+	       System.out.println("Adress: Acer Computer, Lab 314");     
+	       System.out.println("Date: April 14, 2022");  
+	       System.out.println("To: Steven On");
+	       System.out.println("Message: Help! I'm trapped inside a computer!");
 	}
 
 }
