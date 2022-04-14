6a75d5fb889c413736356f28b510b95e4f732c1b
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-14 15:17:42 -0600

    sdad

--- Credit 3/src/Excercises/Excercise2.java
+++ Credit 3/src/Excercises/Excercise2.java
@@ -3,7 +3,10 @@ package Excercises;
 public class Excercise2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+//This is a single-line comment
+/*This is a multi-line comment*/
+
 
 	}
 
