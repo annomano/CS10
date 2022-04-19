0c6683e29e7f7c40b5cfbe0d4528681ff0fc39ba
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-19 14:33:32 -0600

    excercise for java variables

--- Credit 3/src/Excercises/ExcerciseJavaVariables.java
+++ Credit 3/src/Excercises/ExcerciseJavaVariables.java
@@ -1,9 +1,28 @@
 package Excercises;
 
-public class ExcerciseJavaVariables {
+public class ExcerciseJavaVariables 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+
+		String carName = "Volvo";
+		int maxSpeed = 120;
+
+		int x = 5;
+		int y = 10;
+		System.out.println(x + y);
+		
+		int x = 5;
+		int y = 10;
+		int z = x + y;
+		System.out.println(z);
+		
+
+		int x = 5,
+			y = 6,
+			z = 50;
+		System.out.println(x + y + z);
 
 	}
 
