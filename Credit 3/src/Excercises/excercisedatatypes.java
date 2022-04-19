f581714cb89ee6d6dfca5b2c581044697e9c1bf4
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-19 14:42:14 -0600

    excercise data types

--- Credit 3/src/Excercises/excercisedatatypes.java
+++ Credit 3/src/Excercises/excercisedatatypes.java
@@ -1,10 +1,22 @@
 package Excercises;
 
-public class excercisedatatypes {
+public class excercisedatatypes 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
+		int myNum = 9;
+		float myFloatNum = 8.99f;
+		char myLetter = 'A';
+		boolean myBool = false;
+		String myText = "Hello World";
+		
+		// Exercise 2: Primitive Data Types
+		
+		
+		double myDouble = 9.78d;
+		int myInt = (int) myDouble;
 	}
 
 }
