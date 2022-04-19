0c95e27ee26b9a9e41392ccf98f0ea3a995ed5a7
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-19 14:52:48 -0600

    ff

--- Credit 3/src/Excercises/excercisejavastrings.java
+++ Credit 3/src/Excercises/excercisejavastrings.java
@@ -1,10 +1,31 @@
 package Excercises;
 
-public class excercisejavastrings {
+public class excercisejavastrings 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
+		String greeting = "Hello";
+		
+		String txt = "Hello";
+		System.out.println(txt	.length());
+		
+		String txt = "Hello";
+		System.out.println(txt.toUpperCase());
+		
+		String firstName = "John ";
+		String lastName = "Doe";
+		System.out.println(firstName + lastName);
+		
+		String firstName = "John ";
+		String lastName = "Doe";
+		System.out.println(firstName.concat(lastName));
+		
+		String txt = "Hello Everybody";
+		System.out.println(txt.indexOf("e"));
+		
+		
 	}
 
 }
