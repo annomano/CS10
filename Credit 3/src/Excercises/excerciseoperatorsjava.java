aff0e0de6fce0978362715f4cf7f112dfd9d5b49
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-19 14:52:57 -0600

    sdfsdfsdfs

---
+++ Credit 3/src/Excercises/excerciseoperatorsjava.java
@@ -0,0 +1,20 @@
+package Excercises;
+
+public class excerciseoperatorsjava 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(10 * 5);
+		
+		System.out.println(10 / 5);
+
+		int x = 10;
+		++x;
+		
+		int x = 10;
+		x +=5;
+		
+	}
+
+}
