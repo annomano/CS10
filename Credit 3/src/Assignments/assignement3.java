1bf4e05c076f6fe3199c6157cac0afeb883c97d1
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-19 14:56:24 -0600

    sfsffd

--- Credit 3/src/Assignments/assignement3.java
+++ Credit 3/src/Assignments/assignement3.java
@@ -1,10 +1,41 @@
 package Assignments;
+/*
 
-public class assignement3 {
+Program: Assignment1.java          Date: 4/14/2022
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+Author: Steven On 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class assignement3 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	       System.out.println("B     I     N     G     O");
+	       System.out.println("1     19    32    46    61");
+	       System.out.println("6     30    35    49    64");
+	       System.out.println("13    21    Free  52    69");
+	       System.out.println("16    27    42    58    71");
+	       System.out.println("9     23    45    60    75");
+		
+	
 	}
 
 }
+
+/* Screen Dump
+
+B     I     N     G     O
+1     19    32    46    61
+6     30    35    49    64
+13    21    Free  52    69
+16    27    42    58    71
+9     23    45    60    75
+
+ 
+ */
