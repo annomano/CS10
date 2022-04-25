52be472ce7ee1f5481ea43e7f4102b56e2366169
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-25 12:49:20 -0600

    ssdff

---
+++ Credit 4(New)/src/excercises/mathexcercise1.java
@@ -0,0 +1,19 @@
+package excercises;
+
+public class mathexcercise1 
+{
+
+	public static void main(String[] args) 
+	{
+		int x = 5;
+		int y = 10;
+		Math.max(x, y);
+		
+		int x = 16;
+		Math.sqrt(x);
+		
+		Math.random();
+
+	}
+
+}
