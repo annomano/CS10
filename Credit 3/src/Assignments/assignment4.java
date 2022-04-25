3480e01d489680c023dbf61c0abf31422e740857
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-25 12:37:02 -0600

    adasdsad

--- Credit 3/src/Assignments/assignment4.java
+++ Credit 3/src/Assignments/assignment4.java
@@ -10,19 +10,21 @@ Course: Computer Science 10
  
 
 */
+
+import java.util.Scanner;
 public class assignment4 
 {
 
 	public static void main(String[] args) 
 	{
-        Scanner sc = new Scanner(System.in);
-     	double size = 0.0;
-     	double cost = 0.0;
- 	System.out.println("\nEnter the diameter of the pizza in inches: ");
-     	size = sc.nextDouble();
-     	cost = ((.05 * size * size) + 1 + .75);
- 	System.out.println("The cost of making the pizza is: $" + cost);
-
+		  Scanner sc = new Scanner(System.in);
+	     	double size = 0.0;
+	     	double cost = 0.0;
+	 	System.out.println("\nEnter the diameter of the pizza in inches: ");
+	     	size = sc.nextDouble();
+	     	cost = ((.05 * size * size) + 1 + .75);
+	 	System.out.println("The cost of making the pizza is: $" + cost);
+	 	
 	}
 
 }
