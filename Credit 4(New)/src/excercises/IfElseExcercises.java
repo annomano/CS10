7ae4ccb281d68a13b074b55ebfcf22da7a9e00cf
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-25 13:15:39 -0600

    dsfdsf

---
+++ Credit 4(New)/src/excercises/IfElseExcercises.java
@@ -0,0 +1,57 @@
+package excercises;
+
+public class IfElseExcercises 
+{
+
+	public static void main(String[] args) 
+	{
+		int x = 50;
+		int y = 10;
+		if
+		 (x > y) {
+		  System.out.println("Hello World");
+		}
+		
+		//
+		
+		int x = 50;
+		int y = 50;
+		if
+		 (x == y) {
+		  System.out.println("Hello World");
+		}
+		
+		//
+		
+		int x = 50;
+		int y = 50;
+		if (x == y) {
+		  System.out.println("Yes");
+		} else {
+		  System.out.println("No");
+		}  
+		
+		//
+		
+		int x = 50;
+		int y = 50;
+		if (x == y) {
+		  System.out.println("1");
+		} else if (x > y) {
+		  System.out.println("2");
+		} else {
+		  System.out.println("3");
+		}  
+		
+		//
+		
+		int time = 20;
+		String result = (time < 18)? "Good day.":"Good evening.";
+		System.out.println(result);
+		
+		
+		
+		
+	}
+
+}
