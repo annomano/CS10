474e0a1e5bb3a89611a45e0690f53ff231417641
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-04-27 12:53:50 -0600

    ghfj

--- Credit 4(New)/src/cr4Assigments/Assignment8.java
+++ Credit 4(New)/src/cr4Assigments/Assignment8.java
@@ -1,10 +1,42 @@
 package cr4Assigments;
+/*
 
-public class Assignment8 {
+Program: Assignment8.java          Date: 4/29/2022
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+Author: Steven On 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+import java.util.Scanner;
+
+public class Assignment8 
+{
+	public static void main(String[] args) 
+	{
+	     Scanner userinput = new Scanner(System.in);
+	       System.out.println("Enter three digits: ");
+	       int digit = userinput.nextInt();
+	       int first = digit / 100;
+	       int second = (digit / 10) %10;
+	       int third = digit % 10;
+	       System.out.println("The hundreds place digit is: " +first);
+	       System.out.println("The tens place digit is: " +second);
+	       System.out.println("The ones place digit is: " +third);
+		
 	}
 
 }
+
+/* Screen Dump
+
+Enter three digits: 
+385
+The hundreds place digit is: 3
+The tens place digit is: 8
+The ones place digit is: 5
+
+ 
+ */
