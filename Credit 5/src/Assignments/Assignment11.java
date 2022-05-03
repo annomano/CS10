d55928afe60fed0735df6dc7e1d00d98022be73d
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-05-03 14:55:01 -0600

    Yay

---
+++ Credit 5/src/Assignments/Assignment11.java
@@ -0,0 +1,92 @@
+/*
+
+Program: Assignment11.java          Date: 5/4/2022
+
+
+
+Author: Steven On 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+package Assignments;
+
+import java.util.Scanner;
+
+public class Assignment11 
+{
+
+	public static void main(String[] args) 
+	{
+
+		Scanner userinput = new Scanner(System.in);
+	       System.out.println("Please enter a 3 digit number: ");
+	       int Number = userinput.nextInt();
+	       int first = (Number / 100);
+	       int second = (Number / 10) % 10;
+	       int third = (Number % 10);
+	       int max = 0;
+	       int min = 0;
+	       if((first < second)&&(third > second)&&(first < third))
+	       {
+	           max = third;  
+	           min = first;  
+	       }
+	       else if((first > second)&&(third < second)&&(first < third))
+	       {
+	           max = first;
+	           min = third;
+	       }
+	       else if ((first > second)&&(third > second)&&(first < third))
+	       {
+	           max = third;
+	           min = second;
+	       }
+	       else if ((first < second)&&(third < second)&&(first > third))
+	       {
+	           max = second;
+	           min = third;
+	       }
+	       else if ((first < second)&&(third < second)&&(first < third))
+	       {
+	           max = second;
+	           min = first;
+	       }
+	       else if ((first > second)&&(third < second)&&(first > third))
+	       {
+	           max = first;
+	           min = second;
+	       }
+	       System.out.println("The largest number of the 3 digits is: " +max+ "");
+	       System.out.println("The smallest number of the 3 digits is: " +min+ "");
+	       int Sum = (first + second + third);
+	       System.out.println("The sum of the 3 digits are: " +Sum+ "");
+	       int Product = (first * second * third);
+	       System.out.println("The product of the 3 digits are: " +Product+ "");
+	       int Average = (Sum / 3);
+	       System.out.println("The average of the 3 digits are: " +Average+ "");
+	    }
+	}
+
+
+	
+
+
+	/* Screen Dump
+	 
+	Please enter any three digit number: 
+	569
+	The largest number of the 3 digits is: 9
+	The smallest number of the 3 digits is: 5
+	The sum of the 3 digits are: 20
+	The product of the 3 digits are: 270
+	The average of the 3 digits are: 6
+
+	 
+	 */
+		
+		
+		
+		
+
