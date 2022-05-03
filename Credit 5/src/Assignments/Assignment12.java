673e0ead431b19975012238c3b27b723ef23b64c
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-05-03 15:26:40 -0600

    asdad

---
+++ Credit 5/src/Assignments/Assignment12.java
@@ -0,0 +1,46 @@
+/*
+
+Program: Assignment11.java          Date: 5/10/2022
+
+
+
+Author: Steven On 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+package Assignments;
+
+import java.util.Scanner;
+
+public class Assignment12 
+{
+
+	public static void main(String[] args)  
+	{
+
+		Scanner userInput = new Scanner(System.in);
+			System.out.println("Please enter a Grade Percentage:");
+			int grade = userInput.nextInt();
+			if (grade >= 90) 
+			{
+				System.out.println("The corresponding grade letter is: A+");
+			} 
+			
+			else if (grade =< 89)
+			{
+				System.out.println("The corresponding grade letter is: A");
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			//Then do da output stuff
+	}
+
+}
