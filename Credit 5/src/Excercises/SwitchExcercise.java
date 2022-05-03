a0783940ba1476f142fa168fabf08785bb7986bb
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-05-03 14:25:43 -0600

    dfgdgf

---
+++ Credit 5/src/Excercises/SwitchExcercise.java
@@ -0,0 +1,39 @@
+package Excercises;
+
+public class SwitchExcercise 
+{
+
+	public static void main(String[] args) 
+	{
+		int day = 2;
+		switch (day) { 
+		case 1:
+		    System.out.println("Saturday");
+		    break;
+		case 2:
+		    System.out.println("Sunday"); 
+		    break;
+		    
+		    //
+		    
+
+int day = 4;
+switch (day) {
+  
+case 1:
+    System.out.println("Saturday");
+    break;
+case 2:
+    System.out.println("Sunday"); 
+    break;
+    default:
+    System.out.println("Weekend");
+}
+//
+		
+		}
+		
+		
+	}
+
+}
