87c67215f6eab715a6c19b21d05afdf147ce8bd8
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-05-04 13:47:57 -0600

    research more stuff

---
+++ Credit 5/src/Assignments/Assignment15.java
@@ -0,0 +1,36 @@
+/*
+
+Program: Assignment13.java          Date: 5/5/2022
+
+
+
+
+Author: Steven On 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+package Assignments;
+import java.util.Scanner;
+public class Assignment15 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner userinput = new Scanner(System.in);
+		int randomNum1 = (int)(Math.random() * 11); 
+		int randomNum2 = (int)(Math.random() * 11); 
+
+
+		
+	}
+
+}
+
+
+/*Screen Dump:
+  
+
+*/
+ 
