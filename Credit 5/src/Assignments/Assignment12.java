7c8a0dd1df450c28a1fed56311f39ee86b787cfe
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-05-04 13:02:41 -0600

    Done

--- Credit 5/src/Assignments/Assignment12.java
+++ Credit 5/src/Assignments/Assignment12.java
@@ -28,19 +28,106 @@ public class Assignment12
 				System.out.println("The corresponding grade letter is: A+");
 			} 
 			
-			else if (grade =< 89)
-			{
+		       else if ((grade < 89)&&(grade > 80))
+		    {
 				System.out.println("The corresponding grade letter is: A");
 			}
+		       else if ((grade < 79)&&(grade > 75))	
+		   {
+		    	System.out.println("The corresponding grade letter is: B+");		
+		   }
+		       else if ((grade < 74)&&(grade > 70))
+		       {
+		           System.out.println("The corresponding grade letter is: B ");
+		       }
+		       else if ((grade < 69)&&(grade > 65))
+		       {
+		           System.out.println("The corresponding grade letter is: C+ ");
+		       }
+		       else if ((grade < 64)&&(grade > 60))
+		       {
+		           System.out.println("The corresponding grade letter is: C ");
+		       }
+		       else if ((grade < 59)&&(grade > 55))
+		       {
+		           System.out.println("The corresponding grade letter is: D+ ");
+		       }
+		       else if ((grade < 54)&&(grade > 51))
+		       {
+		           System.out.println("The corresponding grade letter is: D ");
+		       }
+		       else if (grade < 50)
+		       {
+		           System.out.println("The corresponding grade letter is: F ");
+		       }  
 			
+			// Used Switch since you can't do : else if ((grade =< 89)
 			
-			
-			
-			
-			
-			
-			
-			//Then do da output stuff
-	}
+			switch (grade) {
+			case 89:
+				System.out.println("The corresponding grade letter is: A");
+				break;
+			case 80:
+				System.out.println("The corresponding grade letter is: A");
+				break;
+			case 79:
+				System.out.println("The corresponding grade letter is: B+");
+				break;
+			case 75:
+				System.out.println("The corresponding grade letter is: B+");
+				break;
+			case 74:
+				System.out.println("The corresponding grade letter is: B");
+				break;
+			case 70:
+				System.out.println("The corresponding grade letter is: B");
+				break;	
+			case 69:
+				System.out.println("The corresponding grade letter is: C+");
+				break;	
+			case 65:
+				System.out.println("The corresponding grade letter is: C+");
+				break;	
+			case 64:
+				System.out.println("The corresponding grade letter is: C");
+				break;	
+			case 60:
+				System.out.println("The corresponding grade letter is: C");
+				break;
+			case 59:
+				System.out.println("The corresponding grade letter is: D+");
+				break;	
+			case 55:
+				System.out.println("The corresponding grade letter is: D+");
+				break;
+			case 54:
+				System.out.println("The corresponding grade letter is: D");
+				break;	
+			case 51:
+				System.out.println("The corresponding grade letter is: D");
+				break;
+			case 50:
+				System.out.println("The corresponding grade letter is: F");
+				break;			
+			}
+				
+				
+		}
 
-}
+  }	    
+			
+/* Screen Dump:
+
+
+Enter a grade percentage: 75
+The corresponding letter grade is: B+
+
+
+
+ */			
+			
+			
+
+	
+
+
