7da15a556eb2d8e5b4a87b120e3797d7de2899b1
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-05-05 14:40:45 -0600

    Done

--- Credit 5/src/Assignments/Assignment14.java
+++ Credit 5/src/Assignments/Assignment14.java
@@ -21,31 +21,34 @@ public class Assignment14
 	public static void main(String[] args) 
 	{
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("Please Enter an Age:");
+		System.out.println("Please enter a age:");
 		int userAge = userInput .nextInt();
-	//Determines what stage of life the user is at based on age
+		
 		if (userAge < 6)
 		{
 			System.out.println("You are a: Toddler");
 		}
-		else if ((userAge >5)&&(userAge < 11))
+		else if ((userAge > 5)&&(userAge < 11)) 
 		{
 			System.out.println("You are a: Child");
 		}
-		else if ((userAge >10)&&(userAge < 13))
+		else if ((userAge > 10)&&(userAge < 13)) 
 		{
-			System.out.println("You are a: Pre-teen");
+			System.out.println("You are a: Pre Teen");
 		}
-		else if ((userAge >12)&&(userAge < 19))
+		else if ((userAge > 12)&&(userAge < 19)) 
 		{
 			System.out.println("You are a: Teenager");
 		}
-		else (userAge > 18)
+		else 
 		{
-			System.out.println("You are a: Adult");
+			System.out.println("You are a: adult");
 		}
-		
-		
+			
+			
+			
+			
+			
 	}
 
 }
--- Credit 5/src/Assignments/Assignment15.java
+++ Credit 5/src/Assignments/Assignment15.java
@@ -1,6 +1,6 @@
 /*
 
-Program: Assignment13.java          Date: 5/5/2022
+Program: Assignment15.java          Date: 5/5/2022
 
 
 
@@ -12,6 +12,7 @@ Course: Computer Science 10
 
 */
 package Assignments;
+import java.util.Random;
 import java.util.Scanner;
 public class Assignment15 
 {
@@ -19,10 +20,51 @@ public class Assignment15
 	public static void main(String[] args) 
 	{
 		Scanner userinput = new Scanner(System.in);
-		int randomNum1 = (int)(Math.random() * 11); 
-		int randomNum2 = (int)(Math.random() * 11); 
+		
+		String operatorSwitch;
+        int b;
+        int c = 0;
 
+        Random number = new Random();
+        int d = number.nextInt(50) +1;
+        int e = number.nextInt(50) +1;
 
+        Random operatorChoice = new Random();
+        int operator = operatorChoice.nextInt(4);
+
+        switch (operator){
+
+            case 0: operatorSwitch= "+";
+                c = d+e;
+                break;
+            case 1: operatorSwitch= "-";
+                c = d-e;
+                break;
+            case 2: operatorSwitch= "*";
+                c = d*e;
+                break;
+            case 3: operatorSwitch= "/";
+                c = d/e;
+                break;
+            default: operatorSwitch= "";
+        }
+        System.out.println("What is: "+d+operatorSwitch+e+"?");
+        b = userinput.nextInt();
+        
+        if (b == c)
+        {
+        	System.out.println("Correct!" +c);
+        
+        }
+        else if (b!=c)
+        {
+        	System.out.println("Wrong. The Correct answer is:" +c);
+        }
+		
+		
+		
+		
+		
 		
 	}
 
@@ -30,7 +72,26 @@ public class Assignment15
 
 
 /*Screen Dump:
-  
+ 
+ 
+ 
+ What is: 23*25?  575
+ Correct! 575
+ 
+ or 
+ 
+What is: 23*25?  2341
+Wrong. The Correct answer is: 575
 
-*/
+/* Screen Dump
+
+What is: 2*39?
+78
+Right answer78
+
+n
+ 
+ */
+
+
  
