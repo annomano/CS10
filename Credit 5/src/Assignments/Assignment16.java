4485ae2c7f07c3f2646206842b9044168a6a34c6
904231004 <904231004@L815WD220025.edu.cbe.ab.ca>
2022-05-05 15:20:00 -0600

    prompt user to enter a number and save it
    use random number to generate it and save it
    
    dispaly both results and if right say congragulations.

---
+++ Credit 5/src/Assignments/Assignment16.java
@@ -0,0 +1,44 @@
+/*
+
+Program: Assignment16.java          Date: 5/10/2022
+
+
+
+
+Author: Steven On 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+package Assignments;
+
+import java.util.Scanner;
+
+
+public class Assignment16 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner userinput = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
+
+// I on team 5
+
+/* Screen Dump:
+
+ 
+ prompt user to enter a number and save it
+
+use random number to generate it and save it
+
+dispaly both results and if right say congragulations.
+ 
+ 
+ */
